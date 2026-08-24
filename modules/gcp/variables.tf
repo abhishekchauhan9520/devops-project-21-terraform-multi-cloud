92fca4fdeefcdb8df5133b231fff09db35c2f4aa
abhishekchauhan9520 <63195310+abhishekchauhan9520@users.noreply.github.com>
2026-08-24 21:03:15 +0530

    feat: add GCP module inputs

---
+++ modules/gcp/variables.tf
@@ -0,0 +1,5 @@
+variable "name" { type = string default = "gcp-demo-vm" }
+variable "machine_type" { type = string default = "e2-micro" }
+variable "zone" { type = string default = "us-central1-a" }
+variable "image" { type = string default = "debian-cloud/debian-12" }
+variable "network_name" { type = string default = "default" }
